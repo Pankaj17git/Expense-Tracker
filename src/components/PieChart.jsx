7463672a5fe591cd70c427911866306c72eaf853
Pankaj <pc620042@gmail.com>
2025-07-28 11:31:09 +0530

    if there is no data the pie chart show there is no data available

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -9,16 +9,40 @@ const settings = {
 };
 
 const DonutChart = ({ data = [], title = '' }) => {
+  // Check if there's at least one non-zero value
+  const hasData = data.some(item => item.value > 0);
+
   return (
     <Box sx={{ textAlign: 'center', mb: 4 }}>
       <Typography variant="subtitle1" sx={{ mb: 1 }}>{title}</Typography>
-      <PieChart
-        sx={{ marginBottom: 2 }}
-        series={[{ innerRadius: 50, outerRadius: 100, data }]}
-        {...settings}
-      />
+
+      {hasData ? (
+        <PieChart
+          sx={{ marginBottom: 2 }}
+          series={[{ innerRadius: 50, outerRadius: 100, data }]}
+          {...settings}
+        />
+      ) : (
+        <Box
+          sx={{
+            width: settings.width,
+            height: settings.height,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: '50%',
+            border: '1px dashed grey',
+            mx: 'auto',
+            color: 'gray',
+            fontSize: '0.9rem'
+          }}
+        >
+          No data available
+        </Box>
+      )}
     </Box>
   );
 };
+
 
 export default DonutChart;
