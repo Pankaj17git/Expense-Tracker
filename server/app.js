20475e317d1058ef401293bdd4bdd74836544f0d
Pankaj <pc620042@gmail.com>
2026-01-17 22:35:21 +0530

    final change

--- server/app.js
+++ server/app.js
@@ -26,13 +26,6 @@ const app = express();
 
 app.use(cors({ origin: "*" }));
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
-
 app.use(helmet({
     crossOriginResourcePolicy: false,
 }));
